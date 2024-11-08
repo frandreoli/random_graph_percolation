4c698798c4e037b84b4cf44e06421b4b42bd4ae6
frandreoli <fra.andreoli@hotmail.it>
2024-11-08 20:15:44 +0100

    Working commit

--- graph.c
+++ graph.c
@@ -1,23 +1,25 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<math.h>
-#define N_0 100
+#define N_NODES 10000
 #define SEED 1651265127
 #define F_FAILURE -1
 #define F_SUCCESS 0
-#define N_GRAPH 20
+#define N_TRAJECTORIES 200
+//Uncomment or comment the following line if you want to respectively delete or keep the trajectory files
 #define DELETE
-#define WIN_OS 0
+//Uncomment the following line if you work in Windows
+#define WIN_OS
 
 
 //DICHIARAZIONI STRUTTURE
 struct connection{unsigned long long int n_bridges; int *bridges;};
 struct graph {struct graph *previous; unsigned long long int size; struct connection neigh;} *nodes;
-struct solution {double mean; double std_dev; double value[N_GRAPH];} read_smax, read_ssqmean;
+struct solution {double mean; double std_dev; double value[N_TRAJECTORIES];} read_smax, read_ssqmean;
 
 
 //VARIABILI GLOBALI
-const unsigned long long int N=N_0;
+const unsigned long long int N=N_NODES;
 unsigned long long int M=0;
 unsigned long long int size_max;
 unsigned long long int size_square_mean;
@@ -82,94 +84,121 @@ else {head1->previous=head2; head2->size=new_size;}
 
 void step(void);
 void step(void){
-unsigned long long int node1=uli_random(0,N), node2;
-do{node2=uli_random(0,N);} while(node1==node2 || check_bridge(node1,node2)==F_FAILURE);
-add_bridge(node1,node2);
-cluster_develop(node1,node2);
-c=2.*((double)M/N);}
+    unsigned long long int node1=uli_random(0,N), node2;
+    do{
+        node2=uli_random(0,N);
+        } while(node1==node2 || check_bridge(node1,node2)==F_FAILURE);
+    add_bridge(node1,node2);
+    cluster_develop(node1,node2);
+    c=2.*((double)M/N);
+}
 
 double std_dev_calc(struct solution);
 double std_dev_calc(struct solution sol){
-double std_dev=0;
-int i;
-for(i=0;i<N_GRAPH;std_dev+=(sol.value[i]-sol.mean)*(sol.value[i++]-sol.mean));
-std_dev=sqrtl(std_dev/(double)(N_GRAPH-1.));
-return std_dev/sqrtl((double)(N_GRAPH)); //dev std sulla media!!
+    double std_dev=0;
+    int i;
+    for(i=0;i<N_TRAJECTORIES;std_dev+=(sol.value[i]-sol.mean)*(sol.value[i++]-sol.mean));
+    std_dev=sqrtl(std_dev/(double)(N_TRAJECTORIES-1.));
+    return std_dev/sqrtl((double)(N_TRAJECTORIES)); 
 }
 
 
 
 //MAIN
 void main(){
-int i=0, j=0, end_file;
-FILE **fp_array, *fp_single;
-char f_string[20], temp;
-#ifdef SEED
-srand(SEED);
-#endif
-#ifndef SEED
-srand(time(0));
-#endif
-char rm_string[4];
-if(WIN_OS)
-    {sprintf(rm_string, "del");}
-else
-    {sprintf(rm_string, "rm");}
+    //Initializing variables
+    int i=0, j=0, end_file;
+    FILE **fp_array, *fp_single;
+    char f_string[20], temp;
+    #ifdef SEED
+    srand(SEED);
+    #endif
+    #ifndef SEED
+    srand(time(0));
+    #endif
+    char rm_string[4];
+    #ifdef WIN_OS
+        {sprintf(rm_string, "del");}
+    #else
+        {sprintf(rm_string, "rm");}
+    #endif
 
-//Alloco memoria
-nodes=(struct graph *)calloc(N, sizeof(struct graph));
-fp_array=(FILE **)calloc(N_GRAPH, sizeof(FILE *));
+    //Allocating memory
+    nodes=(struct graph *)calloc(N, sizeof(struct graph));
+    fp_array=(FILE **)calloc(N_TRAJECTORIES, sizeof(FILE *));
 
-//Calcolo traiettorie
-for(i=0;i<N_GRAPH;i++){
-initialization();
-sprintf(f_string,"Data//graph%d.dat",i);
-if((fp_single=fopen(f_string,"w+"))==NULL) error("ERROR: CANNOT OPEN DATA FILE");
-fprintf(fp_single,"#Smax       S^2mean       c\n");
-for(c=0;c<2;){step();
-fprintf(fp_single,"%g        %g        %g\n", (double)size_max/N, (double)(size_square_mean-(unsigned long long int)size_max*size_max)/N, c);}
-fclose(fp_single);}
+    //Calculating the values for each trajectory
+    for(i=0;i<N_TRAJECTORIES;i++){
+        //Initializing the graph
+        initialization();
+        //Choosing the file name to store the trajectory data 
+        sprintf(f_string,"Data\\graph%d.dat",i);
+        //Opening the file to store the trajectory data 
+        if((fp_single=fopen(f_string,"w+"))==NULL) error("ERROR: I cannot open (w+) the data files to store the trajectories.");
+        //Saving the labels of the data matrix
+        fprintf(fp_single,"#Smax       S^2mean       c\n");
+        //Saving the trajectory data in the data matrix
+        for(c=0;c<2;){
+            step();
+            fprintf(fp_single,"%g        %g        %g\n", (double)size_max/N, (double)(size_square_mean-(unsigned long long int)size_max*size_max)/N, c);
+            }
+        //Closing the file
+        fclose(fp_single);
+    }
 
-printf("\n\nHERE I AMMMMMMMMMMMMMMMM");
+    //Opening the trajectory files in read only mode
+    for(i=0;i<N_TRAJECTORIES;i++){
+        //Defining the file name
+        sprintf(f_string,"Data//graph%d.dat",i);
+        //Opening and checking open result
+        if((fp_array[i]=fopen(f_string,"r"))==NULL){
+            printf("\n\nFILE ERROR: %s",f_string); 
+            error("ERROR: I cannot open (r) the trajectory files to read them.");
+            }
+    }
 
-//Imposto puntatori e buffer di lettura
-for(i=0;i<N_GRAPH;i++){
-sprintf(f_string,"Data//graph%d.dat",i);
-if((fp_array[i]=fopen(f_string,"r"))==NULL) 
-{printf("\n\nFILE ERROR: %s",f_string); error("ERROR: CANNOT OPEN-READ DATA FILE");}
-}
-sprintf(f_string,"g_mean_%Ld.dat",N);
-if((fp_single=fopen(f_string, "w+"))==NULL) error("ERROR: CANNOT OPEN-WRITE DATA FILE");
-fprintf(fp_single,"#Smax_mean          <S^2>_mean          c           dev_Smax_mean       dev_<S^2>_mean\n");
-for(i=0;i<N_GRAPH;i++){
-do{fscanf(fp_array[i],"%c",&temp);}while(temp!='\n');}
+    //Defining the file name of the file where the average quantities willl be saved
+    sprintf(f_string,"Data//g_mean_%d.dat",N);
+    //Opening the file with the average quantities in write mode
+    if((fp_single=fopen(f_string, "w+"))==NULL) error("ERROR: I cannot open (w+) the file to store the average results.");
+    //Saving the labels of the data matrix
+    fprintf(fp_single,"#Smax_mean          <S^2>_mean          c           dev_Smax_mean       dev_<S^2>_mean\n");
+    //Reading the trajectories
+    for(i=0;i<N_TRAJECTORIES;i++){
+        do{
+            fscanf(fp_array[i],"%c",&temp);
+            }while(temp!='\n');
+        }
 
-//Leggo e calcolo la media, e la dev standard
-read_smax.mean=0.; read_ssqmean.mean=0.; read_smax.std_dev=0.; read_ssqmean.std_dev=0.;
-for(end_file=0; end_file!=EOF; read_smax.mean=0., read_ssqmean.mean=0., read_smax.std_dev=0., read_ssqmean.std_dev=0.){
-/*scorro i file dati*/
-end_file=fscanf(fp_array[0], "%Lf   %Lf    %Lf", &read_smax.value[0], &read_ssqmean.value[0], &c);
-read_smax.mean+=read_smax.value[0];
-read_ssqmean.mean+=read_ssqmean.value[0];
-for(i=1;i<N_GRAPH;i++){
-fscanf(fp_array[i], "%Lf   %Lf    %*f", &read_smax.value[i], &read_ssqmean.value[i]);
-read_smax.mean+=read_smax.value[i];
-read_ssqmean.mean+=read_ssqmean.value[i];}
-/*calcolo media*/read_smax.mean/=(double)N_GRAPH;
-read_ssqmean.mean/=(double)N_GRAPH;
-/*calcolo dev std della media*/read_smax.std_dev=std_dev_calc(read_smax);
-read_ssqmean.std_dev=std_dev_calc(read_ssqmean);
-if(end_file!=EOF)
-fprintf(fp_single, "%Lg  %Lg  %Lg  %Lg  %Lg\n", read_smax.mean, read_ssqmean.mean, c, read_smax.std_dev, read_ssqmean.std_dev);}
+    //Computing the average and standard deviations among trajectories
+    read_smax.mean=0.; 
+    read_ssqmean.mean=0.; 
+    read_smax.std_dev=0.; 
+    read_ssqmean.std_dev=0.;
+    for(end_file=0; end_file!=EOF; read_smax.mean=0., read_ssqmean.mean=0., read_smax.std_dev=0., read_ssqmean.std_dev=0.){
+    /*scorro i file dati*/
+    end_file=fscanf(fp_array[0], "%Lf   %Lf    %Lf", &read_smax.value[0], &read_ssqmean.value[0], &c);
+    read_smax.mean+=read_smax.value[0];
+    read_ssqmean.mean+=read_ssqmean.value[0];
+    for(i=1;i<N_TRAJECTORIES;i++){
+    fscanf(fp_array[i], "%Lf   %Lf    %*f", &read_smax.value[i], &read_ssqmean.value[i]);
+    read_smax.mean+=read_smax.value[i];
+    read_ssqmean.mean+=read_ssqmean.value[i];}
+    /*calcolo media*/read_smax.mean/=(double)N_TRAJECTORIES;
+    read_ssqmean.mean/=(double)N_TRAJECTORIES;
+    /*calcolo dev std della media*/read_smax.std_dev=std_dev_calc(read_smax);
+    read_ssqmean.std_dev=std_dev_calc(read_ssqmean);
+    if(end_file!=EOF)
+    fprintf(fp_single, "%Lg  %Lg  %Lg  %Lg  %Lg\n", read_smax.mean, read_ssqmean.mean, c, read_smax.std_dev, read_ssqmean.std_dev);}
 
-//Cancello files
-for(i=0;i<N_GRAPH;i++){
-fclose(fp_array[i]);
-#ifdef DELETE
-sprintf(f_string,"%s Data//graph%d.dat",rm_string, i);
-if(system(f_string)==-1) error("ERROR: CANNOT DELETE FILE");
-#endif
-}
-fclose(fp_single);
+    //Deleting the files with the trajectories only if instructed to do so
+    for(i=0;i<N_TRAJECTORIES;i++){
+        fclose(fp_array[i]);
+        #ifdef DELETE
+        sprintf(f_string,"%s Data\\graph%d.dat",rm_string, i);
+        if(system(f_string)==-1) error("ERROR: I cannot delete the trajectory files");
+        #endif
+    }
+    fclose(fp_single);
 
 }
